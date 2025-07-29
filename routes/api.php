07ec7cece310b5eeb12ffab83dc41f9caf008978
Abdulrhman Ahmed <tknwtknwlwjy10@gmail.com>
2025-07-30 01:03:17 +0300

    change hook method to any

--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\API\BbbEventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/bbb/events', \App\Http\Controllers\API\BbbEventController::class);
+Route::any('/bbb/events', \App\Http\Controllers\API\BbbEventController::class);
